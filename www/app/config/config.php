851dca687b81b08f27c6f084849bf37b81364f0b
Pasha Frolov <pfrlvgg@gmail.com>
2021-12-12 17:13:01 +0300

    fix: kirby-vite plugin paths

--- www/app/config/config.php
+++ www/app/config/config.php
@@ -2,8 +2,9 @@
 
 return [
   # core values
-  'arnoson.kirby-vite.rootDir' => '/../src/',
-  'arnoson.kirby-vite.outDir' => '/assets/',
+  'arnoson.kirby-vite.rootDir' => '../src/',
+  'arnoson.kirby-vite.outDir'  => 'assets',
+  'arnoson.kirby-vite.entry'   => 'app.js',
 
   # optional values
   'debug' => true,
