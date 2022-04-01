74f5697784f33dd27678d2681bb9940386c2d4f0
Pasha Frolov <pfrlvgg@gmail.com>
2022-04-01 13:17:19 +0300

    wip: add used libs

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 import Alpine from 'alpinejs'
 
+// Prevent scrolling while optionally allowing scrolling on specified elements
+// https://github.com/bameyrick/prevent-scrolling
+import { PreventScrolling, ReEnableScrolling } from 'prevent-scrolling'
+
+window.utils = {}
+
+window.utils.PreventScrolling = PreventScrolling
+window.utils.ReEnableScrolling = ReEnableScrolling
+
 Alpine.start()
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,10 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio')
+  ],
 }
