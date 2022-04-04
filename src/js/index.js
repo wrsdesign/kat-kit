d24eaa9100715b5a9f3b6a654fb355b81057d00e
Pasha Frolov <pfrlvgg@gmail.com>
2022-04-04 16:47:57 +0300

    docs: update js comments

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,12 @@ import intersect from '@alpinejs/intersect'
 import LazyLoad from 'vanilla-lazyload'
 
 // Prevent scrolling while optionally allowing scrolling on specified elements
-// git: https://github.com/bameyrick/prevent-scrolling
+// Git: https://github.com/bameyrick/prevent-scrolling
 import { PreventScrolling, ReEnableScrolling } from 'prevent-scrolling'
 
 
 // Force Scroll to the Top of the Page on Page Reload
-// source: https://www.designcise.com/web/tutorial/how-to-force-scroll-to-the-top-of-the-page-on-page-reload-using-javascript
+// Details: https://www.designcise.com/web/tutorial/how-to-force-scroll-to-the-top-of-the-page-on-page-reload-using-javascript
 history.scrollRestoration
   ? (history.scrollRestoration = 'manual')
   : (window.onbeforeunload = () => window.scrollTo(0, 0))
